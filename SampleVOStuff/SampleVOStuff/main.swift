1d1a2729540d51864a02b80bd2c3f2d04f81d017
Felix-Xaver Aigner <felixaigner@Felix-Xavers-MacBook-Air.local>
2021-09-03 18:35:04 +0200

    initial Sample Project commit

---
+++ SampleVOStuff/SampleVOStuff/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  SampleVOStuff
+//
+//  Created by Felix-Xaver Aigner on 03.09.21.
+//
+
+typealias (String, String) -> () = twoStringVoidClosure
+
+let closuretest = 
+
+import Foundation
+
+print("Hello, World!")
+
